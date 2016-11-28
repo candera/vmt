b327246b9bbbeab762e3d33021eb474a1434ba45
Craig Andera <candera@wangdera.com>
2016-11-28 15:00:34 -0600

    Ability to rename cells in formula-of.
    formula-of macro now accepts either a vector or a map in the arguments
    position. If a map, the keys are cells and the values are symbols to
    bind the values of those cells to within the body.

--- src/weathergen/cljs/macros.clj
+++ src/weathergen/cljs/macros.clj
@@ -12,14 +12,29 @@
 (defmacro formula-of
   "Emits a form that will produce a cell using the formula over the
   specified input cells. Avoids some of the code-walking problems of
-  the Hoplon macros."
+  the Hoplon macros. Cells can be either a vector, in which case the
+  cells will be re-bound to their values under the same names within
+  `body`, or a map whose keys are binding forms and whose values are
+  the cells to bind.
+
+  E.g.
+  (formula-of [x y z] (+ x y z))
+
+  (formula-of
+    {foo x
+     {:keys [a b]} y}
+    (+ x a b))"
   [cells & body]
-  `((javelin.core/formula
-     (fn ~cells
-       ~@body))
-    ~@cells))
+  (if (map? cells)
+    `((javelin.core/formula
+       (fn ~(-> cells keys vec)
+         ~@body))
+      ~@(vals cells))
+    `((javelin.core/formula
+       (fn ~cells
+         ~@body))
+      ~@cells)))
 
 (defmacro defformula
   [name cells & body]
   `(def ~name (formula-of ~cells ~@body)))
-
