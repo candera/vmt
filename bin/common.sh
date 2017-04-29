b27519b3bab2855076c423ea3226c47ec2dd1a51
Craig Andera <candera@wangdera.com>
2017-04-29 14:38:00 -0400

    Run `npm install` before packaging.

--- bin/common.sh
+++ bin/common.sh
@@ -1,5 +1,12 @@
 function package () {
     boot electron
+    cd target/
+    # Annoyingly, it seems that if I don't do an `npm install`, the
+    # node_modules directory in the target does not get populated, and
+    # electron-packager does not subsequently include modules in the
+    # package, which causes a runtime error.
+    npm install
+    cd ..
     rm -rf package/
     mkdir -p package/
     ELECTRON_VERSION=$(electron --version | cut -d v -f 2)
