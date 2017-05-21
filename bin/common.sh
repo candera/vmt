49902306a2f80c6f5a376a2d29760d2f666cdc34
Craig Andera <candera@wangdera.com>
2017-05-21 09:41:37 -0400

    Use new icons.

--- bin/common.sh
+++ bin/common.sh
@@ -11,7 +11,7 @@ function package () {
     mkdir -p package/
     ELECTRON_VERSION=$(electron --version | cut -d v -f 2)
     echo "Using Electron version ${ELECTRON_VERSION}"
-    electron-packager target/ --electron-version=$ELECTRON_VERSION --out package/ --overwrite --platform=darwin --icon "assets/images/1stVFW_Insignia.icns"
-    electron-packager target/ --electron-version=$ELECTRON_VERSION --out=package/ --overwrite --platform=win32 --arch=ia32,x64 --icon="assets/images/1stVFW_Insignia.ico" #--asar=true
+    electron-packager target/ --electron-version=$ELECTRON_VERSION --out package/ --overwrite --platform=darwin --icon "assets/images/vmt.icns"
+    electron-packager target/ --electron-version=$ELECTRON_VERSION --out=package/ --overwrite --platform=win32 --arch=ia32,x64 --icon="assets/images/vmt.ico" #--asar=true
 
 }
